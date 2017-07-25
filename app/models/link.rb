f9f29862c838c97c8a0f16ab1388f4f09851e749
Cong Cong Bo <cong@hippocampus.ai>
2017-07-25 16:28:29 +0100

    tag added

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,6 +7,7 @@ class Link
   property :id,     Serial
   property :title,  String
   property :url,    String
+  property :tag,    String
 end
 
 postgres_database = "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,12 +1,28 @@
 feature 'Adding links' do
-  scenario 'I can add new links' do
+  before do
     visit '/links/new'
     fill_in 'title', with: 'Geek Stall'
     fill_in 'url', with: 'http://geekstall.com'
+    fill_in 'tag', with: 'shopping'
     click_button 'Add link'
+  end
+
+  scenario 'I can add titles to new links' do
     expect(current_path).to eq '/links'
     within 'ul#links' do
       expect(page).to have_content('Geek Stall')
+    end
+  end
+
+  scenario 'I can add urls to new links' do
+    within 'ul#links' do
+      expect(page).to have_content('geekstall.com')
+    end
+  end
+
+  scenario 'I can add tags to new links' do
+    within 'ul#links' do
+      expect(page).to have_content('shopping')
     end
   end
 end
