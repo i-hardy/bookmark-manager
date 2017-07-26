5cab352e5027ba4558a747a1ce5040c6cafa361c
Imogen Hardy <imogen.hardy@gmail.com>
2017-07-26 14:31:29 +0100

    enabled validation of user email addresses

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -16,4 +16,12 @@ feature 'User sign up' do
     expect(current_path).to eq('/users')
     expect(page).to have_content 'Password and confirmation password do not match'
   end
+
+  scenario "Can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario "Can't sign up without valid email address" do
+    expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+  end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,4 +6,4 @@ def sign_up(email: 'alice@example.com',
     fill_in :password, with: password
     fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
-  end
+end
